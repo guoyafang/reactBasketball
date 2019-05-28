6d67203a5dfc9e575db02f4353be7d1a484ff4e9
郭雅芳 <wb-gyf390312@alibaba-inc.com>
2019-05-28 11:42:52 +0800

    删除本地创建的home文件夹 方便react项目分离出config文件

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Home from './views/Home/home.jsx';
+
 function App() {
   return (
     <div className="App">
-    <Home/>
-      {/* <header className="App-header">
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header> */}
+      </header>
     </div>
   );
 }
