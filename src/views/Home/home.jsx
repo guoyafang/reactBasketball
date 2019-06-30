982e52f441b7629a076d17a4f724a28beda8173b
amy <shihong.lei@ikang.com>
2019-06-30 14:58:27 +0800

    添加home页面中的变量aaa、bbb 更改app.css文件中的样式

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,31 +1,29 @@
-import  React,{Component} from 'react';
-import Navbar from "./NavBar/navbar.jsx";
-import '../../Mock/mockHome.jsx';
-import axios from 'axios';
+import React, { Component } from 'react'
+import Navbar from './NavBar/navbar.jsx'
+import '../../Mock/mockHome.jsx'
+import axios from 'axios'
 import './home.less'
-class Home extends Component{
-    constructor(){
-        super();
-        this.state={
-            dataList:[]
-        }
-        axios.get('/data',{dataType:'json'}).then(res=>{
-            this.setState({dataList:res.data.list})
-        })
+class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      dataList: [],
+      aaa: [],
+      bb: []
     }
-    render(){
-        return (
-            <div className="homeBox">
-                <Navbar/>
-                    {
-                        this.state.dataList.map((item,index)=>
-                                <div key={index}>
-                                    {item.id}
-                                </div>
-                        )
-                    }
-            </div>
-        )
-    }
+    axios.get('/data', { dataType: 'json' }).then(res => {
+      this.setState({ dataList: res.data.list })
+    })
+  }
+  render() {
+    return (
+      <div className="homeBox">
+        <Navbar />
+        {this.state.dataList.map((item, index) => (
+          <div key={index}>{item.id}</div>
+        ))}
+      </div>
+    )
+  }
 }
-export default Home;
+export default Home
